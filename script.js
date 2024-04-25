f6c19948b4d451635c9272f9eafbaf1c07fa959e
0adev <ayoubaguirar44@gmail.com>
2024-04-25 16:18:25 +0100

    drag and drop project with vanilla JavaScript

---
+++ script.js
@@ -0,0 +1,33 @@
+const lists = document.getElementsByClassName("list");
+const leftSide = document.querySelector(".left");
+const rightSide = document.querySelector(".right");
+
+for (item of lists) {
+  const draglistItem = (e) => {
+    let selected = e.target;
+
+    const dragOverItem = (e) => {
+      e.preventDefault();
+    };
+    rightSide.addEventListener("dragover", dragOverItem);
+
+    const dropItem = () => {
+      rightSide.appendChild(selected);
+      selected = null;
+    };
+    rightSide.addEventListener("drop", dropItem);
+
+    const dragOverItem2 = (e) => {
+      e.preventDefault();
+    };
+    leftSide.addEventListener("dragover", dragOverItem2);
+
+    const dropItem2 = () => {
+      leftSide.appendChild(selected);
+      selected = null;
+    };
+    leftSide.addEventListener("drop", dropItem2);
+  };
+
+  item.addEventListener("dragstart", draglistItem);
+}
