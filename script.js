da93fad56a12257240fe1b4b76ad65622c42ad49
0adev <ayoubaguirar44@gmail.com>
2024-04-26 19:29:02 +0100

    modified script.js

--- script.js
+++ script.js
@@ -1,33 +1,3 @@
-// const lists = document.querySelectorAll(".list");
-// const leftSide = document.querySelector(".left");
-// const rightSide = document.querySelector(".right");
-// let selected = null; // Declaring selected outside the function for better scope
-
-// function dragOverItem(e) {
-//   e.preventDefault();
-// }
-
-// function dropItem(side) {
-//   return function () {
-//     if (selected) {
-//       side.appendChild(selected);
-//       selected = null;
-//     }
-//   };
-// }
-
-// leftSide.addEventListener("dragover", dragOverItem);
-// rightSide.addEventListener("dragover", dragOverItem);
-
-// leftSide.addEventListener("drop", dropItem(leftSide));
-// rightSide.addEventListener("drop", dropItem(rightSide));
-
-// for (item of lists) {
-//   item.addEventListener("dragstart", function (e) {
-//     selected = e.target;
-//   });
-// }
-
 const listItems = document.querySelectorAll(".item");
 const leftSide = document.querySelector(".left");
 const rightSide = document.querySelector(".right");
